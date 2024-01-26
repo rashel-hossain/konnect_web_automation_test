583cfe7ce394ec6e8cbb764238769195f44f51ce
rasel497 <rasel.csediu44@gmail.com>
2024-01-26 23:05:40 +0600

    check login module

---
+++ src/test/java/mypackage/myclass.java
@@ -0,0 +1,44 @@
+package mypackage;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class myclass {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        // Website declaration
+        driver.get("https://konnect.edu.bd/login");
+        driver.manage().window().maximize();
+
+        // login user and password
+        driver.findElement(By.id("username")).sendKeys("01614033999");
+        Thread.sleep(2000); // after 3s por password input filed bosbe
+        driver.findElement(By.id("password")).sendKeys("123456");
+        driver.findElement(By.id("pills-tabContent")).click();
+
+        // check website title
+        String ExpectedTitle= "কিশোর বাতায়ন";
+        String ActualTitle= driver.getTitle();
+        if(ActualTitle.equals(ExpectedTitle)){
+            System.out.println("Title match");
+        }
+        else {
+            System.out.println("Title doesn't match!");
+        }
+
+        // Check website url
+        String ExpectedURL= "https://konnect.edu.bd/";
+        String ActualURL= driver.getCurrentUrl();
+        if(ActualURL.equals(ExpectedURL)){
+            System.out.println("URL Matched");
+        }
+        else {
+            System.out.println("URL doesn't matched!");
+        }
+    }
+}
